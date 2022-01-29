8c85130a257ad90a1e3cf4dd84c86faa36b8c3db
pbenderson <souza98120@gmail.com>
2022-01-29 14:34:49 -0300

    Initial commit

---
+++ JavaScript/aula03/exercicio/index.js
@@ -0,0 +1,3 @@
+var name = 'Enderson'
+
+console.log(`Meu nome é ${name}, Estou aprendendo JavaScript as 15hs da tarde `)
---
+++ JavaScript/aula07/index.js
@@ -0,0 +1,13 @@
+// Nao pode modificar valor de uma constante
+// Nao ultilize var, Utilize Let
+
+const PrimeiroNumero = 5;
+const SegundoNumero = 10;
+
+const Result = PrimeiroNumero * SegundoNumero;
+const ResultDuplicado = Result * 10;
+let ResultTriplicado = Result * 50;
+
+console.log(PrimeiroNumero + ' Primeiro numero');
+console.log(Result + ' Resultado');
+console.log(ResultDuplicado + ' Resultado Duplicado');
